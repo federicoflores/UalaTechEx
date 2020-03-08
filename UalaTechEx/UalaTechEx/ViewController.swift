c8544ae4de7cf0d207be7ecb455da4e7b1cd39c5
Federico Flores <federico.flores.88@hotmail.com>
2020-03-08 17:03:09 -0300

    Initial Commit

---
+++ UalaTechEx/UalaTechEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UalaTechEx
+//
+//  Created by Federico Flores on 8/3/20.
+//  Copyright © 2020 Federico Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
